50c62761b3030db7639118a5d9f5de5102c43187
Daniel Scherzer <daniel@wikiteq.com>
2026-03-03 14:56:52 -0800

    Add support for MediaWiki 1.43.5 (#1)
    Requires updating the WrappedPermissionsLookup check to handle the different
    code path used for core 1.43.5; given how coupled the checks are to the code
    paths used in 1.39.15 and 1.43.5, there should be no issue having them both
    present.
    
    Additionally:
    
    - update composer dependency versions
    - suppress phan complaint on 1.39.15 about a class that does not exist
    - update composer.json in CI to ignore a vulnerable version of PHPUnit required
    by core
    
    HD-4

--- src/WrappedPermissionsLookup.php
+++ src/WrappedPermissionsLookup.php
@@ -1,7 +1,12 @@
 <?php
 
+// We run Phan with both MW 1.39 and MW 1.43, the suppression from 1.39 isn't
+// needed on 1.43 and phan would complain about that
+// @phan-file-suppress UnusedPluginSuppression,UnusedPluginFileSuppression
+
 namespace MediaWiki\Extension\MixedVisibilityFiles;
 
+use MediaWiki\FileRepo\AuthenticatedFileEntryPoint;
 use MediaWiki\Permissions\GroupPermissionsLookup;
 use MediaWiki\Permissions\PermissionManager;
 
@@ -14,6 +19,19 @@ class WrappedPermissionsLookup extends GroupPermissionsLookup {
 	}
 
 	public function groupHasPermission( string $group, string $permission ): bool {
+		// 1.43.5:
+		// Intercept JUST the call in AuthenticatedFileEntryPoint::execute()
+		// to `GroupPermissionsLookup::groupHasPermission( '*', 'read' )`
+		// and return false
+		if ( $group === '*'
+			&& $permission === 'read'
+			// Exists for 1.43.5; class does not need to exist for `::class`
+			// to work
+			// @phan-suppress-next-line PhanUndeclaredClassReference
+			&& wfGetCaller() === AuthenticatedFileEntryPoint::class . '->execute'
+		) {
+			return false;
+		}
 		return $this->original->groupHasPermission( $group, $permission );
 	}
 
@@ -26,6 +44,7 @@ class WrappedPermissionsLookup extends GroupPermissionsLookup {
 	}
 
 	public function getGroupPermissions( array $groups ): array {
+		// 1.39.15:
 		// Intercept JUST the call in img_auth.php to
 		// `$permissionManager->getGroupPermissions( [ '*' ] )` which
 		// the permission manager delegates to the group permissions lookup,
