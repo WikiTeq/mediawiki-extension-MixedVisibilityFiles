50a32e1b12e0fea34b10111bbdf843cd65503061
Pavel Astakhov <pastakhov@yandex.ru>
2026-08-07 08:55:33 +0500

    Drop MediaWiki 1.39; whitelist 1.43.5 || 1.43.8
    Remove img_auth hook path only used on 1.39.15. Pin CI Composer to 2.8.12.

--- src/WrappedPermissionsLookup.php
+++ src/WrappedPermissionsLookup.php
@@ -1,14 +1,9 @@
 <?php
 
-// We run Phan with both MW 1.39 and MW 1.43, the suppression from 1.39 isn't
-// needed on 1.43 and phan would complain about that
-// @phan-file-suppress UnusedPluginSuppression,UnusedPluginFileSuppression
-
 namespace MediaWiki\Extension\MixedVisibilityFiles;
 
 use MediaWiki\FileRepo\AuthenticatedFileEntryPoint;
 use MediaWiki\Permissions\GroupPermissionsLookup;
-use MediaWiki\Permissions\PermissionManager;
 
 class WrappedPermissionsLookup extends GroupPermissionsLookup {
 
@@ -19,15 +14,11 @@ class WrappedPermissionsLookup extends GroupPermissionsLookup {
 	}
 
 	public function groupHasPermission( string $group, string $permission ): bool {
-		// 1.43.5:
 		// Intercept JUST the call in AuthenticatedFileEntryPoint::execute()
 		// to `GroupPermissionsLookup::groupHasPermission( '*', 'read' )`
 		// and return false
 		if ( $group === '*'
 			&& $permission === 'read'
-			// Exists for 1.43.5; class does not need to exist for `::class`
-			// to work
-			// @phan-suppress-next-line PhanUndeclaredClassReference
 			&& wfGetCaller() === AuthenticatedFileEntryPoint::class . '->execute'
 		) {
 			return false;
@@ -44,20 +35,6 @@ class WrappedPermissionsLookup extends GroupPermissionsLookup {
 	}
 
 	public function getGroupPermissions( array $groups ): array {
-		// 1.39.15:
-		// Intercept JUST the call in img_auth.php to
-		// `$permissionManager->getGroupPermissions( [ '*' ] )` which
-		// the permission manager delegates to the group permissions lookup,
-		// and remove the `read` right
-		if ( $groups === [ '*' ]
-			&& wfGetCaller() === PermissionManager::class . '->getGroupPermissions'
-			&& wfGetCaller( 3 ) === 'wfImageAuthMain'
-		) {
-			return array_diff(
-				$this->original->getGroupPermissions( $groups ),
-				[ 'read' ]
-			);
-		}
 		return $this->original->getGroupPermissions( $groups );
 	}
 
